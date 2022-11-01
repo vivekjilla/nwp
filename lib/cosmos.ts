f2ed7b75c0e89b82193fc7a86b3dad618f412dc0
sgollapudi77 <85578033+sgollapudi77@users.noreply.github.com>
2022-11-01 11:43:25 +0530

    Remove access key

--- lib/cosmos.ts
+++ lib/cosmos.ts
@@ -1,7 +1,7 @@
 import {CosmosClient} from "@azure/cosmos";
 
 const endpoint = process.env.COSMOS_ENDPOINT || "https://blog-comments-azure-friday.documents.azure.com:443/";
-const key = process.env.COSMOS_KEY || "tQ8LEe5IEJcUxlKEVXUIqjV6rJhJ7LzBHBz2i11mljIiLDHiUpJWbMdZdIDQ5T1cpvq2KU5fgO38ACDbuXpnCQ==";
+const key = process.env.COSMOS_KEY;
 
 const client = new CosmosClient({endpoint, key});
 
